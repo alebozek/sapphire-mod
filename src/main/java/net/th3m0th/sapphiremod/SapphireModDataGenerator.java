fcff4386cc3c68961a101c5b8487881b0e132411
Alonso Fernández <145675608+af3d3z@users.noreply.github.com>
2024-03-20 20:08:43 +0100

    Migrated every reference from tutorialmod to sapphiremod

--- src/main/java/net/th3m0th/sapphiremod/SapphireModDataGenerator.java
+++ src/main/java/net/th3m0th/sapphiremod/SapphireModDataGenerator.java
@@ -1,9 +1,9 @@
-package net.th3m0th.tutorialmod;
+package net.th3m0th.sapphiremod;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class TutorialModDataGenerator implements DataGeneratorEntrypoint {
+public class SapphireModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
--- src/main/java/net/th3m0th/sapphiremod/block/ModBlocks.java
+++ src/main/java/net/th3m0th/sapphiremod/block/ModBlocks.java
@@ -1,4 +1,4 @@
-package net.th3m0th.tutorialmod.block;
+package net.th3m0th.sapphiremod.block;
 
 import net.fabricmc.fabric.api.block.v1.FabricBlock;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
@@ -11,21 +11,21 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
-import net.th3m0th.tutorialmod.TutorialMod;
+import net.th3m0th.sapphiremod.SapphireMod;
 
 public class ModBlocks {
     public static final Block SAPPHIRE_BLOCK = registerBlock("sapphire_block", new Block(FabricBlockSettings.copyOf(Blocks.DIAMOND_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
 
     public static final Block RAW_SAPPHIRE_BLOCK = registerBlock("raw_sapphire_block", new Block(FabricBlockSettings.copyOf(Blocks.DIAMOND_ORE).sounds(BlockSoundGroup.SPORE_BLOSSOM)));
     private static Block registerBlock(String name, Block block) {
-        return Registry.register(Registries.BLOCK, new Identifier(TutorialMod.MOD_ID, name), block);
+        return Registry.register(Registries.BLOCK, new Identifier(SapphireMod.MOD_ID, name), block);
     }
     private static Item registerBlockItem(String name, Block block) {
-        return Registry.register(Registries.ITEM, new Identifier(TutorialMod.MOD_ID, name), new BlockItem(block, new FabricItemSettings()));
+        return Registry.register(Registries.ITEM, new Identifier(SapphireMod.MOD_ID, name), new BlockItem(block, new FabricItemSettings()));
     }
 
     public static void registerModBlocks() {
-        TutorialMod.LOGGER.info("Registering ModBlocks for " + TutorialMod.MOD_ID);
+        SapphireMod.LOGGER.info("Registering ModBlocks for " + SapphireMod.MOD_ID);
         registerBlockItem("sapphire_block", SAPPHIRE_BLOCK);
         registerBlockItem("raw_sapphire_block", RAW_SAPPHIRE_BLOCK);
     }
