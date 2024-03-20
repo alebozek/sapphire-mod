008a1e7ba060b668bc1dc8c6537ccd6e6527a8cd
th3-m0th <alonsofernandezfernandez64@gmail.com>
2024-03-20 12:56:20 +0100

    Initial commit

---
+++ src/main/java/net/th3m0th/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.th3m0th.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
