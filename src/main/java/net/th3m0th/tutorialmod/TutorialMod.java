cca3ccad8a5f7c14971692f26a8d84de68e7c841
th3-m0th <alonsofernandezfernandez64@gmail.com>
2024-03-20 14:09:36 +0100

    Added sapphires

--- src/main/java/net/th3m0th/tutorialmod/TutorialMod.java
+++ src/main/java/net/th3m0th/tutorialmod/TutorialMod.java
@@ -2,15 +2,23 @@ package net.th3m0th.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
+	public static final Item SAPPHIRE = new Item(new FabricItemSettings());
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
+		Registry.register(Registries.ITEM, new Identifier("sapphire", "sapphire"), SAPPHIRE);
 	}
+
 }
